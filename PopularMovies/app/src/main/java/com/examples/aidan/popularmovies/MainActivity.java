0b1e53a9467adc5a6d983f8f86fad94ab4c4a96f
ClassyMan <aidan.yardy@chp.co.uk>
2016-08-02 19:33:29 -0700

    Initial commit of barebones app

---
+++ PopularMovies/app/src/main/java/com/examples/aidan/popularmovies/MainActivity.java
@@ -0,0 +1,23 @@
+package com.examples.aidan.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        Picasso.with(getApplicationContext())
+                .load(R.drawable.bond)
+                .into(imageView);
+    }
+
+
+}
